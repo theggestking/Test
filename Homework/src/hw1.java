363329cf9c3dba7872264196c3ba1abe081f1767
theggestking <qarqashadze313@gmail.com>
2024-12-14 21:01:05 +0400

    test end - GG

--- Homework/src/hw1.java
+++ Homework/src/hw1.java
@@ -7,31 +7,28 @@ public class hw1 {
 		Scanner scan = new Scanner(System.in);
 
 		System.out.println("Enter your number");
-		if(!scan.hasNextInt()) {
+		if (!scan.hasNextInt()) {
 			System.out.println("Invalid input");
 			scan.close();
 			return;
 		}
 		int number = scan.nextInt();
 		scan.close();
-		
 
 		if (isPrime(number))
 			System.out.println("Your number is prime");
 		else
 			System.out.println("Your number isn't prime");
-System.out.println("hi");
-System.out.println();
+
 	}
-	
+
 	public static boolean isPrime(int num) {
-         
-		if(num == 2) 
+
+		if (num == 2)
 			return true;
 		if (num <= 1 || num % 2 == 0)
 			return false;
 
-		
 		for (int i = 3; i <= Math.sqrt(num); i += 2) {
 			if (num % i == 0) {
 				return false;

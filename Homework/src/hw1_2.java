e1f18b7a20c71453376ae19d70fd5614fb7c80d3
theggestking <qarqashadze313@gmail.com>
2024-12-14 23:43:10 +0400

    added functions

---
+++ Homework/src/hw1_2.java
@@ -0,0 +1,25 @@
+
+public class hw1_2 {
+
+	public static int factorialOf(int num) {
+
+		if (num < 0) {
+			System.out.println("bruh");
+return 0;
+		}
+		if (num == 0 || num == 1) {
+			System.out.println(1);
+			return 1;
+		}
+		int value = 1;
+
+		for (int i = 1; i <= num; i++) {
+			value *= i;
+
+		}
+		System.out.println(value);
+
+		return value;
+	}
+
+}
---
+++ Homework/src/test_area.java
@@ -0,0 +1,12 @@
+
+public class test_area {
+
+	public static void main(String[] args) {
+		
+		hw1.isPrime(4);
+		hw1_2.factorialOf(4);
+		
+		
+	}
+
+}
